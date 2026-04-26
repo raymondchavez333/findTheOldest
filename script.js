d8db7d71d12c93dd825c50c60a7798b86ca9f8f9
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2026-04-26 23:13:50 +0800

    Upload 2 more objects and the initial function

--- script.js
+++ script.js
@@ -15,4 +15,43 @@ const people = [
         yearOfDeath: 1941,
       },
     ]
-    
+
+const people2 = [
+      {
+        name: "Carly",
+        yearOfBirth: 2018,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+
+const people3 = [
+      {
+        name: "Carly",
+        yearOfBirth: 1066,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+
+function findTheOldest (people){
+    return people.reduce(function(accumulator, item){
+
+    }, {});
+}
